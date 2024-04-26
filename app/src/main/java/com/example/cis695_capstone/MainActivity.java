4623ddf512424ee9f6ea1eb1d68385214b43ecdb
Nolan Eitzen <neitzen@my365.bellevue.edu>
2024-04-25 21:29:14 -0500

    Redid Summary UI. Finished BMI and Weight Loss to Date functions

--- app/src/main/java/com/example/cis695_capstone/MainActivity.java
+++ app/src/main/java/com/example/cis695_capstone/MainActivity.java
@@ -19,16 +19,17 @@ import androidx.core.view.WindowInsetsCompat;
 import java.io.Serializable;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private List<entry> history = new ArrayList<entry>();
-    private int beginningWeight = 0;
-    private int goalWeight = 0;
+    private int beginningWeight = 200;
+    private int goalWeight = 180;
     private boolean gender = true;
-    private int height = 0;
+    private int height = 180;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,12 +37,23 @@ public class MainActivity extends AppCompatActivity {
         if(getIntent().getBundleExtra("history") != null){
             Bundle args = getIntent().getBundleExtra("history");
             history = (ArrayList) args.getSerializable("history");
+        } else {
+            // Todo: Rework into startup method
+            Calendar cal = Calendar.getInstance();
+            int year = cal.get(Calendar.YEAR);
+            int month = cal.get(Calendar.MONTH);
+            int day = cal.get(Calendar.DAY_OF_MONTH);
+            String monthStr = numToMonth(month);
+            StringBuilder build = new StringBuilder();
+            build.append(monthStr+ " ");
+            build.append(day + " ");
+            build.append(year + " ");
+            history.add(0, new entry(beginningWeight, build.toString()));
         }
         updateHistory(getIntent().getStringExtra("date"), getIntent().getIntExtra("weight", -1));
         renderBMI();
         renderToDate();
         renderLatestWeight();
-        renderLastFiveWeight();
     }
 
     // The method will act as the logic performed when buttons on the Summary Screen are pressed.
@@ -66,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case "progressPicButton":
                 Log.d("picture button","pressed add progress picture button");
-                // TODO: Future assignmen
+                // TODO: Future assignment
                 break;
             case "appSettingsButton":
                 Log.d("settings button","pressed settings menu button");
@@ -78,17 +90,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void renderBMI(){
-
+        if(!history.isEmpty()){
+            int currentWeight = history.get(history.size() - 1).getWeight();
+            double weightConvert = currentWeight * 0.453592;
+            double heightConvert = height * .01;
+            double bmi = weightConvert / (heightConvert * heightConvert);
+            ((TextView)findViewById(R.id.bmiGenerated)).setText(String.format("%.2f", bmi));
+        } else ((TextView)findViewById(R.id.bmiGenerated)).setText(Double.toString(beginningWeight / (height * height)));
     }
 
     private void renderToDate(){
         if(!history.isEmpty()){
-            int highest = 0;
-            for(entry i : history){
-                if(i.getWeight() > highest) highest = i.getWeight();
-            }
-            ((TextView)findViewById(R.id.toDateGenerated)).setText("You've lost " + Integer.toString(highest-beginningWeight) + "pounds!");
-        } else ((TextView)findViewById(R.id.toDateGenerated)).setText("You haven't started yet. Good Luck!");
+            int weightLost = beginningWeight - history.get(history.size() - 1).getWeight();
+            if(weightLost > 0)((TextView)findViewById(R.id.toDateGenerated)).setText("You've lost " + weightLost + " pounds!");
+             else ((TextView)findViewById(R.id.toDateGenerated)).setText("You've gained " + weightLost*-1 + " pounds!");
+        } else ((TextView)findViewById(R.id.toDateGenerated)).setText("You haven't started yet.");
     }
 
     private void renderLatestWeight(){
@@ -98,22 +114,6 @@ public class MainActivity extends AppCompatActivity {
             );
         }
     }
-
-    private void renderLastFiveWeight() {
-        StringBuilder temp = new StringBuilder();
-        if (!history.isEmpty()) {
-            if(history.size() > 5){
-                for (int x = history.size(); x > history.size() - 5; x--) {
-                    temp.append(history.get(x - 1).toString()).append("\n");
-                }
-            } else
-                for (int x = history.size(); x > 0; x--) {
-                    temp.append(history.get(x - 1).toString()).append("\n");
-                }
-            ((TextView) findViewById(R.id.weightHistoryDataText)).setText(temp.toString());
-        }
-    }
-
     private void updateHistory(String date, int weight){
         if(weight == -1) return;
         else{
@@ -121,7 +121,47 @@ public class MainActivity extends AppCompatActivity {
             history.add(temp);
         }
     }
-}
+
+    private String numToMonth(int month){
+            if(month == 1){
+                return "JAN";
+            }
+            if(month == 2){
+                return "FEB";
+            }
+            if(month == 3){
+                return "MAR";
+            }
+            if(month == 4){
+                return "APR";
+            }
+            if(month == 5){
+                return "MAY";
+            }
+            if(month == 6){
+                return "JUN";
+            }
+            if(month == 7){
+                return "JUL";
+            }
+            if(month ==8){
+                return "AUG";
+            }
+            if(month == 9){
+                return "SEP";
+            }
+            if(month == 10){
+                return "OCT";
+            }
+            if(month == 11){
+                return "NOV";
+            }
+            if(month == 12){
+                return "DEC";
+            }
+            return "Error";
+        }
+    }
 class entry implements Serializable {
     private int weight;
     private String date;
